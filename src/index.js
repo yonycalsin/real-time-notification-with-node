324f7d2a09690dff646de07b6ca159fd4e556eb0
yonicb <yonicb20@gmail.com>
2020-01-18 22:30:11 -0500

    perf: ⚡️ Import routes, morgan, static and express server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const morgan = require("morgan")
+const path = require("path")
+
+const app = express()
+
+// Middleware Sections
+app.use(morgan("dev"))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Routes
+app.use(require("./routes/index"))
+
+// Statict Content
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, ()=> {
+    console.log('Server started');
+})
