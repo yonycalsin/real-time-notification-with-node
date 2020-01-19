428de5e5937c20a1c47782c9484cdaec57eae7e8
yonicb <yonicb20@gmail.com>
2020-01-19 00:00:11 -0500

    style: 💄 Created keys for webpush, and export

--- src/webpush.js
+++ src/webpush.js
@@ -1,1 +1,11 @@
 const webpush = require("web-push")
+
+const { PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY } = process.env
+
+webpush.setGCMAPIKey('<Your GCM API Key Here>');
+webpush.setVapidDetails(
+  'mailto:helloyonicb@gmail.com',
+  PUBLIC_VAPID_KEY,
+  PRIVATE_VAPID_KEY
+);
+module.exports = webpush
